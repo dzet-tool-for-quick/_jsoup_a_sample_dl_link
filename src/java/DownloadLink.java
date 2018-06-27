d92a47cc9f7c92ad46da84bdaeca6a4851bec4a6
littleOStar <qqduxingzhe@163.com>
2018-06-27 16:12:48 +0800

    update

--- src/java/DownloadLink.java
+++ src/java/DownloadLink.java
@@ -15,8 +15,18 @@ public class DownloadLink {
     }
 
     private static void printDownloadLink(String url) throws IOException {
-        Document doc = Jsoup.connect(url + "/tree/master/file").get();
-        Elements aEles = doc.select("#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.file-wrap > table > tbody:nth-child(2) > tr:gt(0) > td:eq(1) > span > a");
+        Document doc = null;
+        Elements aEles = null;
+
+        do {
+            doc = Jsoup.connect(url + "/tree/master/file").get();
+            String html = doc.html();
+//            System.out.println(html);
+            aEles = doc.select("#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > div.file-wrap > table > tbody:nth-child(2) > tr:gt(0) > td:eq(1) > span > a");
+        }while (aEles.size() == 0);
+
+        System.out.println(aEles.size());
+        System.out.println();
 
         for (Element aEle : aEles) {
             String title = aEle.attr("title");
@@ -28,7 +38,7 @@ public class DownloadLink {
             if (exitPartString) {
                 System.out.print(title.substring(length - 9, length - 4));
             }else {
-                System.out.print("part1");
+                System.out.print("part");
             }
             System.out.print("](");
             System.out.print(href);
